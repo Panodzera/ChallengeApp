6c4bd8a96e3404e42b974b002660a571d35be867
Panodzera <matveevdmitriy80@gmail.com>
2023-07-30 17:51:44 +0200

    Zadanie Domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var age = 33;
+var gender = "Kobieta";
+
+if (gender == "Kobieta")
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, 33 lat");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age >= 30)
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+}
+else
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
