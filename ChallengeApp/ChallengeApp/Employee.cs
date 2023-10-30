3497523961588fc5ac8d092440985f38a033f606
Panodzera <matveevdmitriy80@gmail.com>
2023-10-30 07:24:58 +0100

    Zadanie Domowe dzień 6

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1
+﻿namespace ChallengeApp
 {
     public class Employee
     {
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,45 +1,44 @@
-﻿using ConsoleApp1;
+﻿using ChallengeApp;
 
-Employee pracownik1 = new Employee("Monika", "Kot", 33);
-Employee pracownik2 = new Employee("Ewa", "Kinga", 27);
-Employee pracownik3 = new Employee("Piotr", "Kubus", 28);
+Employee employee1 = new Employee("Monika", "Kot", 33);
+Employee employee2 = new Employee("Ewa", "Kinga", 27);
+Employee employee3 = new Employee("Piotr", "Kubus", 28);
 
-pracownik1.AddScore(5);
-pracownik1.AddScore(2);
-pracownik1.AddScore(5);
-pracownik1.AddScore(7);
-pracownik1.AddScore(1);
+employee1.AddScore(5);
+employee1.AddScore(9);
+employee1.AddScore(4);
+employee1.AddScore(7);
+employee1.AddScore(8);
 
-pracownik2.AddScore(8);
-pracownik2.AddScore(9);
-pracownik2.AddScore(4);
-pracownik2.AddScore(5);
-pracownik2.AddScore(3);
+employee2.AddScore(8);
+employee2.AddScore(9);
+employee2.AddScore(7);
+employee2.AddScore(5);
+employee2.AddScore(6);
 
-pracownik3.AddScore(4);
-pracownik3.AddScore(5);
-pracownik3.AddScore(6);
-pracownik3.AddScore(7);
-pracownik3.AddScore(8);
+employee3.AddScore(5);
+employee3.AddScore(6);
+employee3.AddScore(7);
+employee3.AddScore(8);
+employee3.AddScore(9);
 
 List<Employee> employees = new List<Employee>()
 {
-    pracownik1, pracownik2, pracownik3
+    employee1, employee2, employee3
 };
 
 int maxResult = -1;
-Employee pracownikWithMaxResult = null;
+Employee employeeWithMaxResult = null;
 
 foreach (var employee in employees)
 {
     if (employee.Result > maxResult)
     {
-        pracownikWithMaxResult = employee;
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
 }
-// dla lepszej czytelnosci dwa razy zrobiony enter
-// C.W ($"....{p.N} enter {p.S} {p.A}... enter ....{p.R} ");
-
-Console.WriteLine($"najlepszy wynik ma: {pracownikWithMaxResult.Name}" +
-    $" {pracownikWithMaxResult.Surname} {pracownikWithMaxResult.Age} lat. " +
-    $"Ilosć punktow: {pracownikWithMaxResult.Result}  ");
+// zrobiony 2 enter dla kompaktności C.W
+Console.WriteLine($"najlepszy wynik ma: {employeeWithMaxResult.Name}" +
+    $" {employeeWithMaxResult.Surname} {employeeWithMaxResult.Age}" +
+    $" lat. Ilosć punktow: {employeeWithMaxResult.Result}");
